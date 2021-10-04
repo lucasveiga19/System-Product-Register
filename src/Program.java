6d78542c1a96366e8921979e84404faf73fbdc03
Lucas Uchôa da Veiga <lucas.veiga@laveiga.com.br>
2021-10-04 09:42:31 -0300

    List and Organization Creation

--- src/Program.java
+++ src/Program.java
@@ -1,17 +1,33 @@
+import Entities.Product;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Program {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter the number of products: ");
+        List<Product> list = new ArrayList<>();
 
-        System.out.println("Product #1 data:");
-        System.out.print("Common, used or imported (c/u/i)? ");
-        System.out.print("Name: ");
-        System.out.print("Price: ");
-        System.out.print("Customs fee: ");
-        System.out.print("Manufacture date (DD/MM/YYYY): ");
+        System.out.print("Enter the number of products: ");
+        int n = input.nextInt();
+
+        for (int i=1; i<=n; i++) {
+            System.out.println("Product #" + i + " data:");
+            System.out.print("Common, used or imported (c/u/i)? ");
+            char ch = input.next().charAt(0);
+            System.out.print("Name: ");
+            input.nextLine();
+            String name = input.nextLine();
+            System.out.print("Price: ");
+            double price = input.nextDouble();
+            System.out.print("Customs fee: ");
+            System.out.print("Manufacture date (DD/MM/YYYY): ");
+        }
 
         System.out.println();
         System.out.println("PRICE TAGS:");
